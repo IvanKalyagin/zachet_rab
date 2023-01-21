6bfc573ee24c6d708b9a6a0974c798fb470b98d6
Ivan_Kalyagin <ivankalyagin2003@yandex.ru>
2023-01-22 01:19:17 +0300

    working controller with base functions

--- app/controllers/game_words_controller.rb
+++ app/controllers/game_words_controller.rb
@@ -1,13 +1,20 @@
 class GameWordsController < ApplicationController
-
+@@time_start = 0
   def input
+    @text_game = "London is the capital of Great Britian"
   end
 
+  def start_game
+    @text_game = "London is the capital of Great Britian"
+    @@time_start = Time.now
+  end
   def result
     @text_game = "London is the capital of Great Britian"
     input_text = params[:n_s]
     @k = 0
     if input_text != nil then
+      @time_end = Time.now
+      @del_time = @time_end - @@time_start
       length_origin = @text_game.length
       length_input = input_text.length
       if length_input == length_origin then
@@ -16,6 +23,8 @@ class GameWordsController < ApplicationController
             @k += 1
           end
         }
+        @wpm = @k/@del_time * 60
+        @wpm.round
       else
         @res = 'Length does not same'
       end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   get 'game_words/input'
   get 'game_words/result'
   post '/result', to: 'game_words#result'
+  post '/start_game', to: 'game_words#start_game'
   get 'game_words/progress'
   devise_for :users
   get 'static_pages/landing_page'
   get 'dashboard', to: 'game_words#input'
+  get 'progress', to: 'game_words#progress'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
