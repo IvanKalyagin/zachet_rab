e789f14bf511fc820a77270edd4815402f03a3a9
Ivan_Kalyagin <ivankalyagin2003@yandex.ru>
2023-01-23 09:59:38 +0300

    Add best results view

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'static_pages/landing_page'
   get 'dashboard', to: 'game_words#input'
   get 'progress', to: 'game_words#progress'
+  get 'table_res', to: 'game_words#table_res'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
